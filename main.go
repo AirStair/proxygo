cff2a3e923e53aac2f3b09b799d174bf2ab75730
AirStair <AirStair@users.noreply.github.com>
2024-09-12 19:38:39 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,17 @@ import (
 	"log"
 	"net"
 	"fmt"
+	"bufio"
+	"net/http"
+	"strings"
 )
+
+func readRequest(raw, scheme string) (*http.Request, error) {
+	r, err := http.ReadRequest(bufio.NewReader(strings.NewReader(raw)))
+	if err != nil { return nil, err }
+	r.RequestURI, r.URL.Scheme, r.URL.Host = "", scheme, r.Host
+	return r, nil
+ }
 
 func main() {
 	// Listen on TCP port 2000 on all available unicast and
@@ -25,11 +35,30 @@ func main() {
 		// The loop then returns to accepting, so that
 		// multiple connections may be served concurrently.
 		go func(c net.Conn) {
-			b, err := io.ReadAll(c)
+			buf, err := io.ReadAll(c)
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Printf("%s", b)
+			myString := string(buf[:])
+			//log.Print(myString)
+			req, err := readRequest(myString, "http")
+			if err != nil {
+				log.Fatal(err)
+			}
+			conn, err := net.Dial("tcp", req.URL.Host)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Fprintf(conn, myString)
+			respBuf, err := io.ReadAll(conn)
+			if err != nil {
+				log.Fatal(err)
+			}
+			myRespString := string(respBuf[:])
+
+			//c.Write(status)
+			log.Print(myRespString)
+
 			// Echo all incoming data.
 			io.Copy(c, c)
 			// Shut down the connection.
